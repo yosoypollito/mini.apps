978a5eee027d85f956a7b5885af5ce325c6bdd4e
Daniel Iglesias <dev.daif@gmail.com>
2023-04-29 19:38:13 -0400

    feat(apps/img.background.remover): Added tailwind and footer component

--- apps/img.background.remover/src/App.tsx
+++ apps/img.background.remover/src/App.tsx
@@ -1,10 +1,11 @@
-import "./App.css";
+import Footer from "./components/footer";
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col p-4 items-center justify-center min-h-screen">
       <div className="App">Remove your image background.</div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ apps/img.background.remover/src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div>
+      <span>
+        Maded by Daif with <a href="https://www.cloudinary.com/">Cloudinary</a>.
+      </span>
+    </div>
+  );
+}
